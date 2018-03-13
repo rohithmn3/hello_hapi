16063818386e5757bacb1cac9b1b4be96ce7991a
Rohith <rohithmn03@gmail.com>
2018-03-13 13:10:32 +0530

    Update task_show_uname.sh

--- task_show_uname.sh
+++ task_show_uname.sh
@@ -3,7 +3,7 @@ echo "************************"
 echo "Rohith"
 echo "************************"
 id
-sudo apt-get update
-sudo apt-get install docker -y
-sudo usermod -aG docker $(whoami)
-sudo docker images
+apt-get update
+apt-get install docker -y
+usermod -aG docker $(whoami)
+docker images
