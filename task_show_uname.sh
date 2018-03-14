945a3630f1e5ae6783d88b516befd7deba2c8151
Rohith <rohithmn03@gmail.com>
2018-03-14 15:08:55 +0530

    Update task_show_uname.sh

--- task_show_uname.sh
+++ task_show_uname.sh
@@ -3,12 +3,4 @@ echo "************************"
 echo "Rohith"
 echo "************************"
 id
-apt-get update
-apt-get install docker.io -y
-usermod -aG docker $(whoami)
-docker-compose up
-echo "################"
-docker
-echo "################"
-docker images
-id
+cat variables/var.txt
